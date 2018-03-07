8ff59713ea948b3331bd3361db3a4a75f120d3aa
andy <andy.zhangtao@hotmail.com>
2018-03-07 09:00:03 +0800

    支持解析github工程的.hicd配置文件
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- agents/gitAgent/main.go
+++ agents/gitAgent/main.go
@@ -6,11 +6,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/andy-zhangtao/humCICD/agents/models"
 	"github.com/urfave/cli"
 	"gopkg.in/src-d/go-git.v4"
 )
@@ -60,6 +64,13 @@ func cloneGit(url, name string) (err error) {
 		URL:      url,
 		Progress: os.Stdout,
 	})
+
+	hc, err := parseConfigure("/tmp/" + name)
+	if err != nil {
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{"configrue": hc}).Info("gitAgent")
 	return
 }
 
@@ -67,5 +78,29 @@ func parseName(url string) (name string) {
 	gitName := strings.Split(url, "/")
 	name = strings.Split(gitName[len(gitName)-1], ".")[0]
 	fmt.Printf("GitAgent Will Clone [%s]\n", name)
+	return
+}
+
+// parseConfigrue 解析工程中的.hicd文件
+// path 工程路径
+func parseConfigure(path string) (configure *models.HicdConfigure, err error) {
+	configure = new(models.HicdConfigure)
+	fileName := path + "/.hicd"
+	_, err = os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil, errors.New(fmt.Sprintf("Open .hicd error[%s]", err))
+	}
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Read .hicd error[%s]", err))
+	}
+
+	logrus.WithFields(logrus.Fields{".hcid": string(data)}).Info("gitAgent")
+
+	err = json.Unmarshal(data, configure)
+	if err != nil {
+		return nil, err
+	}
 	return
 }
---
+++ agents/models/configure.go
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018.
+ * andy-zhangtao <ztao8607@gmail.com>
+ */
+
+package models
+
+type HicdConfigure struct {
+	Version string     `json:"version"` //当前配置文件版本
+	Kind    string     `json:"kind"`    //语言类型
+	Depend  []HiDepend `json:"depend"`  //依赖数据
+}
+
+type HiDepend struct {
+	Kind string   `json:"kind"` //依赖类型 docker, shell etc
+	Exec string   `json:"exec"` //执行命令内容, 如果是docker则是镜像名称, 如果是shell，则是shell 命令
+	Args []string `json:"args"` //命令参数
+}
